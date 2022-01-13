aaae57215329406170e891c7ff3a7b864facfbcc
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-01-13 06:41:46 -0300

    Update and rename pombo.py to carinhas.py

---
+++ plugins/carinhas.py
@@ -0,0 +1,57 @@
+import asyncio
+import os
+import random
+import re
+import requests
+import wget
+import datetime
+import math
+from cowpy import cow
+from random import randint, choice
+
+from kannax import Message, kannax
+
+
+@kannax.on_cmd("carinhas", about={"header": "carinhas"})
+async def snake_(message: Message):
+    """memes fodas"""
+    out = f"""
+( ͡❛ ͜ʖ ͡❛)⠀⠀           
+"""
+
+    out2 = f"""
+(. ͡❛ ͜ʖ ͡❛.)
+"""
+    out3 = f"""
+【 ͡❛ ͜ʖ ͡❛】
+"""
+
+    out4 = f"""
+¯\_( ͡❛ ͜ʖ ͡❛)_/¯⠀
+"""
+    
+    out5 = f"""
+¯\_( ͡ಠ ͜ʖ ͡ಠ)_/¯
+     """
+        
+    out6 = f"""
+¯\_( ͡ಥ ͜ʖ ͡ಥ)_/¯⠀
+     """
+    
+    out7 = f"""
+ ¯\_( ͡◉ ͜ʖ ͡◉)_/¯
+    """
+    
+    await message.edit(out)
+    await asyncio.sleep(2)
+    await message.edit(out2)
+    await asyncio.sleep(2)
+    await message.edit(out3)
+    await asyncio.sleep(2)
+    await message.edit(out4)
+    await asyncio.sleep(2)
+    await message.edit(out5)
+    await asyncio.sleep(2)
+    await message.edit(out6)
+    await asyncio.sleep(2)
+    await message.edit(out7)
