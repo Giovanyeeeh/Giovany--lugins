65f5490629ffe3521e760900e7a73d9de02431a1
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-01-14 10:31:55 -0300

    Create kek.py

---
+++ plugins/kek.py
@@ -0,0 +1,8 @@
+@kannax.on_cmd(r"(?:Kek|:/)$",
+               about={'header': "Check yourself, hint: :P"}, name='Kek',
+               trigger='', allow_via_bot=False)
+async def kek_(message: Message):
+    """hue"""
+    kek = [":p", ":D"]
+    for i in range(1, 9):
+        await message.try_to_edit(":" + kek[i % 2])
